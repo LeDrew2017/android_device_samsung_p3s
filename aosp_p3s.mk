f661b33b06cacde6e2f47c2d62a8f3c14bbd8ecb
TQMatvey <maybeetube@gmail.com>
2023-02-14 13:54:17 +0700

    p3s: fix boot animation resolution

--- aosp_p3s.mk
+++ aosp_p3s.mk
@@ -25,7 +25,7 @@ $(call inherit-product, device/samsung/p3s/device.mk)
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Pixel Experience Flags
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := false # This is correct, device doesn't support Quick Tap
